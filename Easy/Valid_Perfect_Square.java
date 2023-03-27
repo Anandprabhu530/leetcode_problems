d6206f2a7e45f48df514cd7d539cdf5f0b772d99
Anandprabhu530 <97842562+Anandprabhu530@users.noreply.github.com>
2023-03-27 21:24:20 +0530

    Add files via upload
    My submission link : https://leetcode.com/problems/valid-perfect-square/solutions/3348310/java-binary-search-solution/

---
+++ Easy/Valid_Perfect_Square.java
@@ -0,0 +1,23 @@
+//367. Valid Perfect Square
+//https://leetcode.com/problems/valid-perfect-square/description/
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1){
+            return true;
+        }
+        int start = 1;
+        int end = num;
+        while(start<=end){
+            long mid = start + (end - start)/2;
+            if(mid*mid==num){
+                return true;
+            }
+            if(mid*mid>num){
+                end = (int)(mid - 1);
+            }else{
+                start = (int)(mid + 1);
+            }
+        }
+        return false;
+    }
+}
