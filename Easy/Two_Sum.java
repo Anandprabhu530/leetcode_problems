dd7819e2395e27c734e40ac9ea94be3553e37889
Anandprabhu530 <97842562+Anandprabhu530@users.noreply.github.com>
2023-03-31 22:04:42 +0530

    Added Two_Sum.java

---
+++ Easy/Two_Sum.java
@@ -0,0 +1,16 @@
+//1. Two Sum
+
+//https://leetcode.com/problems/two-sum/description/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i!=j && (nums[i] + nums[j])==target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
