d318f4b26185633d21f732f6e6bc805078a4d031
Anandprabhu530 <97842562+Anandprabhu530@users.noreply.github.com>
2023-04-30 20:50:08 +0530

    Added Range_Sum_of_BST.java

---
+++ Medium/Range_Sum_of_BST.java
@@ -0,0 +1,39 @@
+//938. Range Sum of BST
+
+//https://leetcode.com/problems/range-sum-of-bst/
+
+//My-Submission: https://leetcode.com/problems/range-sum-of-bst/solutions/3470309/easy-java-solution/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null){
+            return 0;
+        }
+        if(low<=root.val && root.val<=high){
+            sum += root.val;
+        }
+        if(root.val>low){
+            rangeSumBST(root.left, low, high);
+        }
+        if(root.val<high){
+            rangeSumBST(root.right, low, high);
+        }
+        return sum;
+    }
+}
